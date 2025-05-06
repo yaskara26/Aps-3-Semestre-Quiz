dc62c784af7f4ad0d3c77258b529010562b4d1df
Jhonatan-Diniz <jhonatandiniz360@gmail.com>
2025-05-05 22:34:55 -0300

    Consertando as dimensões da tela

--- script.js
+++ script.js
@@ -191,40 +191,6 @@ document.getElementById("finalizar-quiz").addEventListener('click', () => {
     enviarQuiz();
     // setTimeout(window.location.reload.bind(window.location), 2000);
 })
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const mapa = document.getElementsByTagName("svg")[0];
-//     console.log(mapa)
-//     // Acessa o conteúdo do SVG
-//     mapa.addEventListener("load", () => {
-  
-//       // Seleciona todos os estados pelo ID ou classe
-//       const estados = mapa.querySelectorAll('[id^="estado-"]');
-  
-//       estados.forEach((estado) => {
-//         // Salva a cor original do estado
-//         const corOriginal = estado.style.fill;
-  
-//         // Adiciona evento de clique
-//         estado.addEventListener("click", () => {
-//             showAside(estado.id);
-//             showBtniniciarQuiz();
-//           alert(`Você clicou no estado: ${estado.id}`);
-//         });
-  
-//         // Adiciona efeito de hover
-//         estado.addEventListener("mouseover", () => {
-//           estado.style.fill = "#77777780";
-//         });
-  
-//         // Retorna à cor original no mouseout
-//         estado.addEventListener("mouseout", () => {
-//           estado.style.fill = corOriginal;
-//         });
-//       });
-//     });
-//   });
-
 document.addEventListener("DOMContentLoaded", () => {
     const mapa = document.getElementById("mapa-brasil");
   
